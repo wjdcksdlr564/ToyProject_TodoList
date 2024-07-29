a88ded95433d60b58ad50da479267c4e30abb6af
choiddanbi <eksql5955@naver.com>
2024-07-29 17:13:13 +0900

    post delete요청 생성

--- todolist_back/src/main/java/com/toyproject/todolist/controller/TodoController.java
+++ todolist_back/src/main/java/com/toyproject/todolist/controller/TodoController.java
@@ -1,13 +1,33 @@
 package com.toyproject.todolist.controller;
 
+import com.toyproject.todolist.dto.ReqRegisterTodoDto;
+import com.toyproject.todolist.entity.Todo;
+import com.toyproject.todolist.service.TodoServiceImpl;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 @Log4j2
 public class TodoController {
+
+    @Autowired
+    private TodoServiceImpl todoService;
+
+    // 등록
+    @PostMapping("/todo")
+    public ResponseEntity<?> registerApi(@RequestBody ReqRegisterTodoDto reqDto) {
+        log.info("{}", reqDto);
+        return ResponseEntity.ok().body(todoService.registerTodo(reqDto));
+    }
+
+    // 삭제
+    @DeleteMapping("/todo/{todoId}")
+    public ResponseEntity<?> deleteApi(@PathVariable int todoId) {
+        log.info("{}", todoId);
+        return ResponseEntity.ok().body(todoService.deleteTodo(todoId));
+    }
 }
--- todolist_back/src/main/java/com/toyproject/todolist/dto/ReqRegisterTodoDto.java
+++ todolist_back/src/main/java/com/toyproject/todolist/dto/ReqRegisterTodoDto.java
@@ -1,7 +1,15 @@
 package com.toyproject.todolist.dto;
 
+import com.toyproject.todolist.entity.Todo;
 import lombok.Data;
 
 @Data
 public class ReqRegisterTodoDto {
+    private String todoName;
+
+    public Todo toEntity() {
+        return Todo.builder()
+                .todoName(todoName)
+                .build();
+    }
 }
